edbc8888ac9e951ceaef47f72512e507bc3b9ddb
EmmaK <emmaculatewkamau@gmail.com>
2022-04-07 14:13:55 +0300

    Hide description when page is loaded the first time

--- src/app/card-detail/card-detail.component.ts
+++ src/app/card-detail/card-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Card } from '../card';
 
 @Component({
   selector: 'app-card-detail',
@@ -7,8 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardDetailComponent implements OnInit {
 
-  constructor() { }
+  @Input() card: Card
 
+  constructor() { }
   ngOnInit(): void {
   }
 
