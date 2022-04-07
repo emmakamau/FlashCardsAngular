c977f4aebbc431b7a752251ddb22c87de137e4c8
EmmaK <emmaculatewkamau@gmail.com>
2022-04-07 14:17:25 +0300

    Add toggle effect, click on title to view description

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -15,6 +15,10 @@ export class CardComponent implements OnInit {
     new Card(5,'Data-binding','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'),
   ]
 
+  toggleDetails(index){
+    this.cards[index].showDescription = !this.cards[index].showDescription;
+  }
+
   constructor() { }
   ngOnInit(): void {
   }
